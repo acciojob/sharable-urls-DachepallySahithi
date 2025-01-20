52d4569665098c182ae2831ae220485c143d12a9
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-20 10:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+    var name = document.getElementById('name').value;
+    var year = document.getElementById('year').value;
+    var url = 'https://localhost:8080/';
+
+    if (name) {
+        url += 'name=' + encodeURIComponent(name);
+    }
+
+    if (year) {
+        if (name) {
+            url += '&';
+        } else {
+            url += '?';
+        }
+        url += 'year=' + encodeURIComponent(year);
+    }
+
+    document.getElementById('url').textContent = url;
+});
